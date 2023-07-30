aa4b7c7a7cd68b6c2aaeaf17a6bc60de6f1c51c6
milogop <milogop@gmail.com>
2023-07-30 11:32:14 +1000

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello World");
+        System.out.println("~~~~~\n" +
+                "Red vs. Blue\n" +
+                "~~~~~\n");
+
+        // test
     }
 }
 
