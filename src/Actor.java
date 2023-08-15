dd720b8f221581513805a8be3ca53b7f2bc019df
milogop <milogop@gmail.com>
2023-08-16 09:55:45 +1000

    Task 11 my answer
    Marked 3/3 for this week as voluntary Viva.

--- src/Actor.java
+++ src/Actor.java
@@ -1,14 +1,27 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Polygon;
+import java.util.ArrayList;
 
 public abstract class Actor {
   Color color;
   Cell loc;
 
+  ArrayList<Polygon> shape = new ArrayList<Polygon>(); 
+  
+
   public void paint(Graphics g) {
-    g.setColor(color);
-    g.fillRect(loc.x + 5, loc.y + 5, loc.width - 10, loc.height - 10);
-    g.setColor(Color.GRAY);
-    g.drawRect(loc.x + 5, loc.y + 5, loc.width - 10, loc.height - 10);
+    // g.setColor(color);
+    // g.fillRect(loc.x + 5, loc.y + 5, loc.width - 10, loc.height - 10);
+    // g.setColor(Color.GRAY);
+    // g.drawRect(loc.x + 5, loc.y + 5, loc.width - 10, loc.height - 10);
+
+    for (Polygon shapes : shape) {
+      g.setColor(color);
+      g.fillPolygon(shapes);
+    }
+
   }
 }
+
+
